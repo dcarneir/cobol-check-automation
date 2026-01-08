9e337103de7497ff450a087ed7d2314f574b42c0
Daniel Carneiro <dcarneir@gmail.com>
2026-01-08 17:35:38 -0300

    partial commit

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -2,7 +2,7 @@
 # zowe_operations.sh
 # Convert username to lowercase
 set -x
-LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+LOWERCASE_USERNAME=$(echo "$ZOWE_OPT_USER" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
